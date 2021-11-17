e120a9ad69f7642f5197c08f07eb39bc4d7aa4c7
Claudioss23 <souza.claudiof23@gmail.com>
2021-11-17 17:01:18 -0300

    Mudanças 2

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -3,9 +3,13 @@ package one.digitalinnovation.personapi.service;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -17,19 +21,22 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
-        Person personToSave = Person.builder()
-                .firstName(personDTO.getFirstName())
-                .lastName(personDTO.getLastName())
-                .cpf(personDTO.getCpf())
-                .birthDate(personDTO.getBirthDate())
-                .phones(personDTO.getPhones())
-                .build();
+        Person personToSave = personMapper.toModel(personDTO);
 
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
